ded2589654a61aa9b39f7ca45102d3cc0bb23fd2
EfremovFedor <149171583+EfremovFedor@users.noreply.github.com>
2023-10-27 16:45:26 +0300

    домашнее задание 1 Си

---
+++ Zadanie1.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hellow World\n");
+	printf("Let`s\n");
+	printf("     go\n");
+	printf("       to walk\n");
+	printf ("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HH HH\n");
+	printf(" ZZZZZ\n");
+	return 0;
+}
+
