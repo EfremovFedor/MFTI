9c10290d649e1e319093fc8e0f5fced5c0db2f6f
EfremovFedor <149171583+EfremovFedor@users.noreply.github.com>
2023-10-29 11:38:30 +0300

    Add files via upload

---
+++ Zadanie1.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Hellow World\n");
+	printf("Let`s\n");
+	printf("     go\n");
+	printf("       to walk\n");
+	printf ("   *\n");
+	printf("  ***\n");
+	printf(" *****\n");
+	printf("*******\n");
+	printf(" HH HH\n");
+	printf(" ZZZZZ\n");
+	printf(" _   _\n");
+	printf("/ \\_/ \\\n");
+	printf("\\_/ \\_/\n");
+	printf("/ \\_/ \\\n");
+	printf("\\_/ \\_/\n");
+	return 0;
+}
+
